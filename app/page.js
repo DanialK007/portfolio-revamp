609d64a41ba1463a7b088deb9743f27543058f8b
DanialK007 <kh2496187@gmail.com>
2025-08-13 23:01:04 +0630

    Version 1.6.9 - build error

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 import About from "../components/About";
 import Footer from "../components/Footer";
 import FadeIn from "../components/framer-motion/FadeIn";
-import Header from "@/components/Header";
+import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Hero2 from "../components/Hero2";
 import Hero3 from "../components/Hero3";
 import Intro from "../components/Intro";
-import PlayGround from "@/components/PlayGround";
+import PlayGround from "../components/PlayGround";
 import { MouseFollower } from "../components/ui/MouseFollower";
 import Work from "../components/Work";
 
