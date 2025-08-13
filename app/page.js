073076a1e101c491b79c063efdca50569f17f005
DanialK007 <kh2496187@gmail.com>
2025-08-13 22:59:45 +0630

    Version 1.6.8 - build error

--- app/page.js
+++ app/page.js
@@ -1,19 +1,19 @@
 import About from "../components/About";
 import Footer from "../components/Footer";
 import FadeIn from "../components/framer-motion/FadeIn";
-import Header from "../components/Header";
+import Header from "@/components/Header";
 import Hero from "../components/Hero";
 import Hero2 from "../components/Hero2";
 import Hero3 from "../components/Hero3";
 import Intro from "../components/Intro";
-import PlayGround from "../components/PlayGround";
+import PlayGround from "@/components/PlayGround";
 import { MouseFollower } from "../components/ui/MouseFollower";
 import Work from "../components/Work";
 
 export default function Home() {
   return (
     <main className="bg-black">
-      {/* <Header /> */}
+      <Header />
       <Hero3 />
       <Intro />
       <Work />
